df6b31caa36efe313976598eb1daf461ba6b034e
Hoang Van Chien <chienhv.ous@mcredit.com.vn>
2022-06-01 11:43:18 +0700

    update

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
     res.sendFile(duongDanFile);
 })
 
-app.listen(port, () => {
-    console.log(`app listening port: ${port}`);
+app.listen(process.env.PORT, () => {
+    // console.log(`app listening port: ${port}`);
 })
