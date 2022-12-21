e14769ee24e34d3e2c99584fa942bddb378c246c
HoangChienxv3 <55954255+HoangChienxv3@users.noreply.github.com>
2022-12-21 14:06:16 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3200;
 const path = require('path');
 
 app.use('/public', express.static(path.join(__dirname, '/public')));
